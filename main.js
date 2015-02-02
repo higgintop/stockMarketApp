7502dcf104ed2cd7e620e6783dbb217e1efb1cd1
Allison Higginbotham <higgintop@gmail.com>
2015-02-02 13:37:03 -0600

    appending to the target

--- main.js
+++ main.js
@@ -4,33 +4,63 @@
 
     var $symbol;
     var url;
+    var $qty;
 
   // initialization function
   $(function init() {
 
     $('#buy').click(function(){
       var $symbol= $('#symbol').val().toUpperCase();
-      var $qty = $("#quantity").val();
+      $qty = $("#quantity").val();
       url = 'http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol='+ $symbol + '&callback=?';
       console.log(url);
 
       $.getJSON(url, function(res){
-        var name = res.Name;
-        var lastPrice = res.LastPrice;
-        var change = res.Change;
-        var changePercent = res.ChangePercent;
 
-        console.log(name, lastPrice, change, changePercent);
+        // call createList with our items
+        $('#target').append(createList(res));
 
       });
 
     });
    
-  });
+  }); // init
+
+  function createList(res){
+    var name = res.Name;
+    var lastPrice = res.LastPrice;
+    lastPrice = Math.round(lastPrice * 100)/100;
+    var change = res.Change;
+    change = Math.round(change * 100)/100;
+    var changePercent = res.ChangePercent;
+    changePercent = Math.round(changePercent * 100)/100;
+
+    var stocks = [];
+
+    
+    var $ul = $('<ul></ul>');
+
+    var $liName = $('<li>' + name + '</li>');
+    var $liLastPrice = $('<li>' + lastPrice + '</li>');
+    var $liQty = $('<li>' + $qty + '</li>');
+    var $liChange = $('<li>' + change + '</li>');
+    var $liChangePercent = $('<li>' + changePercent + '</li>');
+    var $liRemove = $('<li><button>Remove</button></li>');
+    $ul.append($liName);
+    $ul.append($liLastPrice);
+    $ul.append($liQty);
+    $ul.append($liChange);
+    $ul.append($liChangePercent);
+    $ul.append($liRemove);
+
+      
+
+    stocks.push($ul);
+  
+
+    return stocks;
 
 
-  function grabData() {
-  }
-
+  }//createList
 
 })();
